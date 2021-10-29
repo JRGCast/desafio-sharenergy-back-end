e78908155efd8c46d23819a89a19aa56f8153602
JRGCast <joao.ralph1@gmail.com>
2021-10-29 19:49:58 -0300

    trying resolve cors problem

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 const PORT = process.env.PORT;
 
 app.get('/', (_req, res) => {
